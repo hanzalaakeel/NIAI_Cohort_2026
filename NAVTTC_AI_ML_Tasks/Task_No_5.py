2f2753ce44feadfb0a12198b9118eca2f0e4556f
Hanzala Akeel <hanzalaakeel@gmail.com>
2026-03-17 23:37:51 +0500

    Square Root of a nummber with and without user input

--- NAVTTC_AI_ML_Tasks/Task_No_5.py
+++ NAVTTC_AI_ML_Tasks/Task_No_5.py
@@ -3,6 +3,6 @@
 import math
 
 # num = 16
-num = int(input("Enter the number: "))
-num_sqrt = math.sqrt(num)
-print(f"The square root of {num} is {num_sqrt}")
+number = int(input("Enter the number: "))
+num_sqrt = math.sqrt(number)
+print(f"The square root of {number} is {num_sqrt}")
