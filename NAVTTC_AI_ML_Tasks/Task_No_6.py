676e72f4b88cc1e6f6364bf93be2dd810d166f33
Hanzala Akeel <hanzalaakeel@gmail.com>
2026-03-17 23:55:39 +0500

    Square Root of a real or complex number with user & without user input

--- NAVTTC_AI_ML_Tasks/Task_No_6.py
+++ NAVTTC_AI_ML_Tasks/Task_No_6.py
@@ -2,7 +2,7 @@
 
 import cmath
 
-num = 1 + 2j
+number = 1 + 2j
 # num = eval(input("Enter the number: "))
-num_sqrt = cmath.sqrt(num)
-print(f"The square root of {num} is {num_sqrt}")
+num_sqrt = cmath.sqrt(number)
+print(f"The square root of {number} is {num_sqrt}")
